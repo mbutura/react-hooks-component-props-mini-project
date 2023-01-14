e0144b5bfbfcce196735cf095b0d65219d7eea1b
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-01-14 23:10:32 +0000

    Implement bonus coffee read mins

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,6 +5,7 @@ function Article({title, date="January 1, 1970", preview, minutes}){
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
+            <small>{"☕️".repeat(Math.ceil(minutes/5)) + `${minutes} min Read`}</small>
             <p>{preview}</p>
         </article>
     );
